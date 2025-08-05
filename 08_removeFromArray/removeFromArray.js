83b59408b7ba320c1a6620d884bc5a080adca381
MateoTVara <146038574+MateoTVara@users.noreply.github.com>
2025-08-05 12:36:24 -0500

    complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...pop) {
+  let filtered = arr.filter((val) => !pop.includes(val));
+  return filtered;
 };
 
 // Do not edit below this line
